a057d3190a30b2b8297e6636bb6649d4afee3743
HongchengQ <121159914+HongchengQ@users.noreply.github.com>
2026-01-04 01:29:35 +0800

    refactor(project): 重构项目模块结构
    - 将 game-server 的 Main.java 移至 game 包下并更新包名
    - 删除 gate-server 中的旧 Main.java 文件
    - 创建新的 common 模块并添加基础配置
    - 更新 game-server 和 gate-server 的 pom.xml 依赖配置
    - 将父项目配置从 xt 改为 root 并更新模块引用
    - 移除父项目中多余的 spring-boot 插件配置
    - 添加 common 模块的测试类和主类文件

---
+++ common/src/main/java/com/nailong/xt/common/test/Test.java
@@ -0,0 +1,7 @@
+package com.nailong.xt.common.test;
+
+public class Test {
+    public static void iop() {
+        System.out.println("common test");
+    }
+}
--- gate-server/src/main/java/com/nailong/xt/gate/Main.java
+++ gate-server/src/main/java/com/nailong/xt/gate/Main.java
@@ -1,9 +1,13 @@
-package com.nailong.xt;
+package com.nailong.xt.gate;
+
+import com.nailong.xt.common.test.Test;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     static void main() {
-        IO.println("gate server start");
+        System.out.println("gate server start");
+
+        Test.iop();
     }
 }
