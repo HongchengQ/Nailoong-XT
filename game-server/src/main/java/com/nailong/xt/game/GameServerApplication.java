6dd5dc43132b76597af34121ad6020858faca071
HongchengQ <121159914+HongchengQ@users.noreply.github.com>
2026-01-04 02:25:39 +0800

    添加proto

---
+++ game-server/src/main/java/com/nailong/xt/game/GameServerApplication.java
@@ -0,0 +1,9 @@
+package com.nailong.xt.game;
+
+import org.springframework.boot.SpringApplication;
+
+public class GameServerApplication {
+    static void main(String[] args) {
+        SpringApplication.run(GameServerApplication.class, args);
+    }
+}
---
+++ gate-server/src/main/java/com/nailong/xt/gate/GateServerApplication.java
@@ -0,0 +1,11 @@
+package com.nailong.xt.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateServerApplication {
+    static void main(String[] args) {
+        SpringApplication.run(GateServerApplication.class, args);
+    }
+}
